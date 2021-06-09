5ed6dad39f4d0520b0b84a878f9b505b51f68476
Michael Slevin <mikeslevin@gmail.com>
2021-06-09 01:24:23 +0100

    Introduced autofixture autodata to tests

--- hotel-booking-tests/BookingManagerTests.cs
+++ hotel-booking-tests/BookingManagerTests.cs
@@ -3,6 +3,7 @@ using System;
 using FluentAssertions;
 using System.Collections.Generic;
 using System.Linq;
+using AutoFixture.NUnit3;
 
 namespace hotel_booking_tests
 {
@@ -13,64 +14,79 @@ namespace hotel_booking_tests
         {
         }
 
-        [Test]
-        public void IsRoomAvailable_Returns_True_if_no_bookings_have_been_made_on_date_given()
+        [Test, AutoData]
+        public void IsRoomAvailable_Returns_True_if_no_bookings_have_been_made_on_date_given(
+            int roomNumber,
+            DateTime date
+        )
         {
-            var rooms = new List<int>() { 101 };
+            var rooms = new List<int>() { roomNumber };
             IBookingManager bm = new BookingManager(rooms);
-            var date = new DateTime(2021,6,7);
-            bm.IsRoomAvailable(101, date).Should().BeTrue();
+            bm.IsRoomAvailable(roomNumber, date).Should().BeTrue();
         }
 
-        [Test]
-        public void IsRoomAvailable_Returns_False_if_a_booking_has_been_made_for_room_date_pair()
+        [Test, AutoData]
+        public void IsRoomAvailable_Returns_False_if_a_booking_has_been_made_for_room_date_pair(
+            int roomNumber,
+            DateTime date
+        )
         {
-            var rooms = new List<int>() { 101 };
+            var rooms = new List<int>() { roomNumber };
             IBookingManager bm = new BookingManager(rooms);
-            var date = new DateTime(2021,6,7);
             bm.AddBooking("Richard", 101, date);
             bm.IsRoomAvailable(101, date).Should().BeFalse();
         }
 
-        [Test]
-        public void AddBooking_Throws_if_a_booking_has_been_made_for_the_room_already()
+        [Test, AutoData]
+        public void AddBooking_Throws_if_a_booking_has_been_made_for_the_room_already(
+            int roomNumber,
+            DateTime date
+        )
         {
-            var rooms = new List<int>() { 101 };
+            var rooms = new List<int>() { roomNumber };
             IBookingManager bm = new BookingManager(rooms);
-            var date = new DateTime(2021,6,7);
-            bm.AddBooking("Richard", 101, date);
-            Action act = () => bm.AddBooking("Twain",101, date);
+            bm.AddBooking("Richard", roomNumber, date);
+            Action act = () => bm.AddBooking("Twain",roomNumber, date);
             act.Should().Throw<RoomUnavailableException>();
         }
 
-        [Test]
-        public void GetAvailableRooms_Returns_all_rooms_when_no_bookings_have_been_made()
+        [Test, AutoData]
+        public void GetAvailableRooms_Returns_all_rooms_when_no_bookings_have_been_made(
+            int room1,
+            int room2,
+            int room3        
+        )
         {
-            var rooms = new List<int>()
-            {
-                1,2,2007
-            };
+            var rooms = new List<int>() {room1, room2, room3};
             IBookingManager bm = new BookingManager(rooms);
             var availableRooms = bm.GetAvailableRooms(new DateTime()).ToList();
-            availableRooms.Contains(1).Should().BeTrue();
-            availableRooms.Contains(2).Should().BeTrue();
-            availableRooms.Contains(2007).Should().BeTrue();
+
+            foreach(var room in rooms)
+            {
+                availableRooms.Contains(room).Should().BeTrue();
+            }
         }
 
-        [Test]
-        public void GetAvailableRooms_Does_Not_Return_Rooms_That_Are_Booked()
+        [Test, AutoData]
+        public void GetAvailableRooms_Does_Not_Return_Rooms_That_Are_Booked(
+            int room1,
+            int room2,
+            int room3,
+            int bookedRoom,
+            DateTime date
+        )
         {
-            var rooms = new List<int>()
-            {
-                1,2,2007
-            };
-            var date = new DateTime(2021,6,7);
-            IBookingManager bm = new BookingManager(rooms);
-            bm.AddBooking("Cyrus", 2007, date);
+            var allRooms = new List<int>() {room1, room2, room3, bookedRoom};
+
+            IBookingManager bm = new BookingManager(allRooms);
+
+            bm.AddBooking("Cyrus", bookedRoom, date);
             var availableRooms = bm.GetAvailableRooms(date).ToList();
-            availableRooms.Contains(1).Should().BeTrue();
-            availableRooms.Contains(2).Should().BeTrue();
-            availableRooms.Contains(2007).Should().BeFalse();
+            
+            availableRooms.Contains(room1).Should().BeTrue();
+            availableRooms.Contains(room2).Should().BeTrue();
+            availableRooms.Contains(room3).Should().BeTrue();
+            availableRooms.Contains(bookedRoom).Should().BeFalse();
         }
     }
 }
